106c23d0c7f17fba03f840448292144d6980712f
Obsessed Cake <aaaoxxx@gmail.com>
2023-06-28 00:13:42 +0300

    Formatting

--- src/db.rs
+++ src/db.rs
@@ -19,14 +19,16 @@ impl DB {
     }
 
     pub fn create_table(&self) -> Result<usize> {
-        Ok(self
-            .conn
-            .execute("CREATE TABLE IF NOT EXISTS logs (block_number INTEGER, log_index INTEGER, log TEXT)", ())?)
+        Ok(self.conn.execute(
+            "CREATE TABLE IF NOT EXISTS logs (block_number INTEGER, log_index INTEGER, log TEXT)",
+            (),
+        )?)
     }
 
     pub fn insert_logs(&self, block_number: u64, log_index: u64, log: &str) -> Result<usize> {
-        Ok(self
-            .conn
-            .execute("INSERT INTO logs(block_number, log_index, log) values (?1, ?2, ?3)", (block_number, log_index, log))?)
+        Ok(self.conn.execute(
+            "INSERT INTO logs(block_number, log_index, log) values (?1, ?2, ?3)",
+            (block_number, log_index, log),
+        )?)
     }
 }
