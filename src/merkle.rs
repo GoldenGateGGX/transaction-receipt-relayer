36a69a5a8b745528502e58874ca8f5034a2eebbb
Obsessed Cake <aaaoxxx@gmail.com>
2023-06-27 23:20:38 +0300

    Add Merkle proofs

---
+++ src/merkle.rs
@@ -0,0 +1,31 @@
+use std::hash::Hash;
+
+use blake2::{Blake2s256, Digest};
+use ethers::types::H256;
+use merkle_cbt::merkle_tree::{CBMT, Merge};
+
+struct MergeH256 {}
+
+impl Merge for MergeH256 {
+    type Item = H256;
+
+    fn merge(left: &Self::Item, right: &Self::Item) -> Self::Item {
+        let left_and_right = left
+            .as_bytes()
+            .iter()
+            .chain(right.as_bytes().iter())
+            .cloned()
+            .collect::<Vec<_>>();
+
+        H256::from_slice(Blake2s256::digest(left_and_right).as_slice())
+    }
+}
+
+#[allow(non_camel_case_types)]
+type CBMT_H256 = CBMT<H256, MergeH256>; 
+
+pub fn verify(hashes: &[H256], indices: &[u32], proof_leaves: &[H256]) -> bool {
+    let root = CBMT_H256::build_merkle_root(hashes);
+    let proof = CBMT_H256::build_merkle_proof(hashes, indices).unwrap();
+    proof.verify(&root, proof_leaves)
+}
