c94914e04e8aa68b4980cd25aede255638deee1b
Obsessed Cake <aaaoxxx@gmail.com>
2023-06-28 19:27:00 +0300

    Add a function to select all logs from DB

--- src/db.rs
+++ src/db.rs
@@ -1,10 +1,14 @@
+use std::sync::{Arc, Mutex};
+
+use ethers::types::Log;
 use eyre::Result;
 use rusqlite::Connection;
 
 use crate::config::Config;
 
+#[derive(Clone)]
 pub struct DB {
-    conn: Connection,
+    conn: Arc<Mutex<Connection>>,
 }
 
 impl DB {
@@ -15,20 +19,37 @@ impl DB {
             Connection::open_in_memory()?
         };
 
-        Ok(DB { conn })
+        Ok(DB { conn: Arc::new(Mutex::new(conn)) })
     }
 
     pub fn create_table(&self) -> Result<usize> {
-        Ok(self.conn.execute(
+        let conn = self.conn.lock().expect("acquire mutex");
+        Ok(conn.execute(
             "CREATE TABLE IF NOT EXISTS logs (block_number INTEGER, log_index INTEGER, log TEXT)",
             (),
         )?)
     }
 
     pub fn insert_logs(&self, block_number: u64, log_index: u64, log: &str) -> Result<usize> {
-        Ok(self.conn.execute(
+        let conn = self.conn.lock().expect("acquire mutex");
+        Ok(conn.execute(
             "INSERT INTO logs(block_number, log_index, log) values (?1, ?2, ?3)",
             (block_number, log_index, log),
         )?)
+    }
+
+    pub fn select_logs(&self) -> Result<Vec<Log>> {
+        let conn = self.conn.lock().expect("acquire mutex");
+        let mut stmt = conn.prepare("SELECT log FROM logs ORDER BY block_number, log_index")?;
+        let raw_logs_iter = stmt.query_map([], |row| {
+            Ok(row.get::<_, String>(0)?)
+        })?;
+        let mut logs = vec![];
+        for raw_log in raw_logs_iter {
+            if let Ok(raw_log) = raw_log {
+                logs.push(serde_json::from_str(&raw_log)?)
+            }
+        }
+        Ok(logs)
     }
 }
