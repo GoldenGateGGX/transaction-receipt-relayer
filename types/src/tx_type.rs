b60d01c5433d1631634a7eaccdc39410ff1dbaf2
Obsessed Cake <aaaoxxx@gmail.com>
2023-08-02 15:24:25 +0300

    Simplification

--- types/src/tx_type.rs
+++ types/src/tx_type.rs
@@ -1,15 +1,10 @@
 use alloy_rlp::Encodable;
 use bytes::BufMut;
 
-const LEGACY_TX_TYPE_ID: u8 = 0;
-const EIP2930_TX_TYPE_ID: u8 = 1;
-const EIP1559_TX_TYPE_ID: u8 = 2;
-const EIP4844_TX_TYPE_ID: u8 = 3;
-
 /// Transaction Type enum; adapted from [`reth_primitives::TxType`][1].
 ///
 /// [1]: https://github.com/paradigmxyz/reth/blob/f41386d28e89dd436feea872178452e5302314a5/crates/primitives/src/transaction/tx_type.rs#L22-L32
-#[derive(Default, Debug, PartialEq)]
+#[derive(Default, Debug, PartialEq, Clone, Copy)]
 pub enum TxType {
     /// Legacy transaction pre EIP-2929
     #[default]
@@ -26,12 +21,7 @@ impl Encodable for TxType {
     /// TxType is encoded as [`u8`][1].
     /// [1]: https://github.com/paradigmxyz/reth/blob/f41386d28e89dd436feea872178452e5302314a5/crates/primitives/src/transaction/mod.rs#L556
     fn encode(&self, out: &mut dyn BufMut) {
-        match self {
-            TxType::Legacy => out.put_u8(LEGACY_TX_TYPE_ID),
-            TxType::EIP2930 => out.put_u8(EIP2930_TX_TYPE_ID),
-            TxType::EIP1559 => out.put_u8(EIP1559_TX_TYPE_ID),
-            TxType::EIP4844 => out.put_u8(EIP4844_TX_TYPE_ID),
-        }
+        out.put_u8(*self as u8)
     }
 
     fn length(&self) -> usize {
